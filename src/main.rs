a015ce8fc9a413abaae4e7f20702cfe682bc0c83
Robin Whittleton <robin.whittleton@digital.cabinet-office.gov.uk>
2016-05-29 18:11:42 +0100

    Fill initial field with random bools

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,19 @@ extern crate rand;
 
 fn main() {
   use rand::Rng;
-  
+
   const WIDTH: usize = 600;
   const HEIGHT: usize = 600;
   const TOTAL_CELLS: usize = WIDTH * HEIGHT;
 
-  let field: [bool; TOTAL_CELLS] = [false; TOTAL_CELLS];
-  let field_buffer: [bool; TOTAL_CELLS] = [false; TOTAL_CELLS];
+  let mut field: [bool; TOTAL_CELLS] = [false; TOTAL_CELLS];
+  let mut field_buffer: [bool; TOTAL_CELLS] = [false; TOTAL_CELLS];
 
+  let mut rng = rand::thread_rng();
 
-  let rng = rand::thread_rng();
-  if rng.gen() { // random bool
-      println!("i32: {}, u32: {}", rng.gen::<i32>(), rng.gen::<u32>())
+  // fill the initial field with a random state
+  for i in 0..field.len() {
+    field[i] = rng.gen::<bool>()
   }
-
-  //println!("{}", field);
 
 }

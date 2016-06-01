d41847ab1e12b20bbf2ca21a330ce33eae987b10
Robin Whittleton <robin.whittleton@digital.cabinet-office.gov.uk>
2016-06-01 09:04:45 +0100

    Integrate Piston for a drawing window
    Red square for demo purposes.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate rand;
+extern crate piston_window;
+
+use piston_window::*;
 
 fn main() {
   use rand::Rng;
@@ -6,6 +9,10 @@ fn main() {
   const WIDTH: usize = 600;
   const HEIGHT: usize = 600;
   const TOTAL_CELLS: usize = WIDTH * HEIGHT;
+
+  let mut window: PistonWindow =
+    WindowSettings::new("Life.rs", [600, 600])
+    .exit_on_esc(true).build().unwrap();
 
   let mut field: [bool; TOTAL_CELLS] = [false; TOTAL_CELLS];
   let mut field_buffer: [bool; TOTAL_CELLS] = [false; TOTAL_CELLS];
@@ -17,4 +24,18 @@ fn main() {
     field[i] = rng.gen::<bool>()
   }
 
+  while let Some(event) = window.next() {
+    //step();
+    draw(&mut window, &event, &field);
+  }
+
+}
+
+fn draw(window: &mut PistonWindow, event: &piston_window::Event, field: &[bool]) {
+  window.draw_2d(event, |c, g| {
+    clear([1.0; 4], g);
+    rectangle([1.0, 0.0, 0.0, 1.0],
+              [0.0, 0.0, 100.0, 100.0],
+              c.transform, g);
+  });
 }
